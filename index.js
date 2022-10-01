afcc5fc9a11c007aa59958a5ddbe3f5354300325
JK1320 <kaurjus1@gmail.com>
2022-10-02 00:45:36 +0100

    import express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello");
+const express = require("express");
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("this is only a test")
+})
+
+app.listen(5000, (req, res)=> {
+    console.log("Server is running");
+})
