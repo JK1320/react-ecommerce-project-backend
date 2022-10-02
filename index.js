c577138d3208536af5b6f4b93a1d6cb2a4361e11
JK1320 <kaurjus1@gmail.com>
2022-10-03 00:00:41 +0100

    import dotenv

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv"); // to use env import dotenv
 
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO_URL)
+.then(() => console.log("DB Connection Successful!"))
+.catch((err) => {
+    console.log(err);
+})
 
 app.get("/", (req, res) => {
     res.send("this is only a test")
 })
 
-app.listen(5000, (req, res)=> {
+app.listen(process.env.PORT || 5000, () => {
     console.log("Server is running");
 })
