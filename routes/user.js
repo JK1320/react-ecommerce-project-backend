f66f60d1e001cbcc41541243b59bc848b7a0f165
JK1320 <kaurjus1@gmail.com>
2022-10-03 13:32:07 +0100

    added POST route

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,12 @@ router.get("/usertest", (req, res) => {
 res.send("user test is successful")
 })
 
+router.post("/userposttest", (req, res) => {
+    const username = req.body.username;
+    console.log(username);
+    res.send(username)
+})
+
 
 
 module.exports = router;
