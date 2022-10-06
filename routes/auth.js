dc67bea2d7365233149d6a111bc5a3c3034329ef
JK1320 <kaurjus1@gmail.com>
2022-10-06 14:54:00 +0100

    added POST route to register user

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,26 @@
 const router = require("express").Router();
+const User = require("../models/User");
+
+// REGISTER
+router.post("/register", async(req, res) => {
+// if(!req.body){
+//     res.status(400).json("please fill in all the details");
+// };
+
+const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+});
+
+
+try {
+    const savedUser = await newUser.save();     // save() will save data in mongoose database
+    res.status(201).json(savedUser);
+} catch (error) {
+    res.status(500).json(error);
+};
+
+ });
 
 module.exports = router;
