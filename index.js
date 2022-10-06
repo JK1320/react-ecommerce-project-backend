ecc7a3c4bd3c9b949955c69547f481bfc86e3969
JK1320 <kaurjus1@gmail.com>
2022-10-06 14:33:00 +0100

    import auth route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv"); // to use env import dotenv
 const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -14,7 +15,11 @@ mongoose
 })
 
 app.use(express.json()); // to post json file from frontend need this 
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+
+
+
 
 
 app.listen(process.env.PORT || 5000, () => {
