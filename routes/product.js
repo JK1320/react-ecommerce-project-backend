952e40154d250a67420b68582a5399100561dcea
JK1320 <kaurjus1@gmail.com>
2022-10-12 22:10:46 +0100

    add PUT route to update product

--- routes/product.js
+++ routes/product.js
@@ -19,6 +19,24 @@ router.post("/", verifyTokenAndAdmin, async(req, res) => {
     }
 })
 
+// Update Product route
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 
 
 module.exports = router;
