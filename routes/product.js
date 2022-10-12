cdc9261f37fca4dc392c168a35f38947399fb2bb
JK1320 <kaurjus1@gmail.com>
2022-10-12 07:14:05 +0100

    add POST route for product

--- routes/product.js
+++ routes/product.js
@@ -1,3 +1,24 @@
+const CryptoJS = require("crypto-js");
+const Product = require("../models/Product");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
 const router = require("express").Router();
+
+//CREATE
+router.post("/", verifyTokenAndAdmin, async(req, res) => {
+    const newProduct = new Product(req.body);
+
+    try {
+        const savedProduct = await newProduct.save();
+        res.status(200).json(savedProduct);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+
 
 module.exports = router;
