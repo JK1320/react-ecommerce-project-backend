57e24725e53bb368b5cd4c66d003d71a7b405d87
JK1320 <kaurjus1@gmail.com>
2022-10-13 13:54:55 +0100

    Add GET route for user cart with userId

--- routes/cart.js
+++ routes/cart.js
@@ -19,7 +19,7 @@ router.post("/", verifyToken, async(req, res) => {
     }
 })
 
-// Update Product route
+// Update Cart route
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
 
   try {
@@ -37,11 +37,22 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   }
 });
 
-//Delete Product route
+//Delete Cart route
 router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
   try {
     await Cart.findByIdAndDelete(req.params.id);
     res.status(200).json("Cart has been deleted...");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//Get user Cart route with ID
+router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const Cart = await Cart.findOne({userId: req.params.userId});
+
+    res.status(200).json(Cart);
   } catch (error) {
     res.status(500).json(error);
   }
