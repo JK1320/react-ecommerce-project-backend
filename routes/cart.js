f47aa25b32809fb438caf04d3395dabaa234fe20
JK1320 <kaurjus1@gmail.com>
2022-10-13 13:43:05 +0100

    Add POST route to create new cart

--- routes/cart.js
+++ routes/cart.js
@@ -1,3 +1,24 @@
+const CryptoJS = require("crypto-js");
+const Product = require("../models/Product");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
 const router = require("express").Router();
+
+//CREATE
+router.post("/", verifyToken, async(req, res) => {
+    const newCart = new Cart(req.body);
+
+    try {
+        const savedCart = await newCart.save();
+        res.status(200).json(savedCart);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+
 
 module.exports = router;
