2bfd45aa91a32e880118ac1cd65fca37a55a8d8d
JK1320 <kaurjus1@gmail.com>
2022-10-13 22:50:49 +0100

    Add POST & PUT routes for order

--- routes/order.js
+++ routes/order.js
@@ -1,3 +1,41 @@
+//const CryptoJS = require("crypto-js");
+const Order = require("../models/Order");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
 const router = require("express").Router();
+
+//CREATE
+router.post("/", verifyToken, async (req, res) => {
+  const newOrder = new Order(req.body);
+
+  try {
+    const savedOrder = await newOrder.save();
+    res.status(200).json(savedOrder);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Update Cart route
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedOder = await Order.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+
+    res.status(200).json(updatedOrder);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+
 
 module.exports = router;
