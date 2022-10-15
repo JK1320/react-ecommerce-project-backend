0386a8da998a56d5c7d2e81463c959d2d3b96bcb
JK1320 <kaurjus1@gmail.com>
2022-10-15 07:29:38 +0100

    import cart routes and order route

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const dotenv = require("dotenv"); // to use env import dotenv
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 
 dotenv.config();
 
@@ -19,6 +21,8 @@ app.use(express.json()); // to post json file from frontend need this
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 
 
