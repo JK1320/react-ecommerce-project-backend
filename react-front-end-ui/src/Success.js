aa419b8f1ac4ddd28c870994cde8683d35a8569c
JK1320 <kaurjus1@gmail.com>
2022-10-18 12:24:07 +0100

    Add Success component

---
+++ react-front-end-ui/src/Success.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Success = () => {
+  return (
+    <div className='success-btn-container'>
+        <button>Successful.</button>
+        <p>Your order is being prepared. Thanks for choosing Lama Shop.</p>
+    </div>
+  )
+}
+
+export default Success;
