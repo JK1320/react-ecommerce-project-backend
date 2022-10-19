59c337af6a3f41acbb7543b52a385fe297f568c0
JK1320 <kaurjus1@gmail.com>
2022-10-19 22:25:57 +0100

    import Router, Route & Switch

--- react-front-end-ui/src/App.js
+++ react-front-end-ui/src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Pay from './Pay';
 import Success from './Success';
 
 
 function App() {
   return (
-    <div className="App">
-    <Pay />
-    <Success />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/pay">
+          <Pay />
+        </Route>
+        <Route path="/success">
+          <Success />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
